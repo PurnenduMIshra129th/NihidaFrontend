62a7fae8293f6505cccb9d5bdd77762de2d32453
Purnendu Mishra <purnendumishra129th@gmail.com>
2025-05-15 19:57:39 +0000

    added port in env variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 import { defineConfig } from 'vite'
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: parseInt(process.env.VITE_PORT || '3000'),
   },
 })
