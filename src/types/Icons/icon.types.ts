2ad3ccd986b1828408f76b4c87eb82644a4d9e24
Purnendu Mishra <purnendumishra129th@gmail.com>
2025-06-05 11:58:02 +0000

    deleted icon types

--- src/types/Icons/icon.types.ts
+++ src/types/Icons/icon.types.ts
@@ -1,10 +1,6 @@
-import {nameOfIcon } from "../../utils/constant";
 
 export interface IIconProps {
     className?: string;
-}
-export interface IIconButtonProps {
-    name: keyof typeof nameOfIcon
 }
 
 export interface IDonorCardIconProps{
